74d32768f2fc1f9bce23047e93399dbefe008eb9
GjorgjievaS <68241215+GjorgjievaS@users.noreply.github.com>
2022-02-20 00:08:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,546 @@
+var dictionary=[
+    [
+        {
+            value:'маса',
+            direction:'horizontal',
+            position:'1-3',
+            description:'Парче мебел што се состои од рамна плоча поставена на четири ногалки'
+        },
+        {
+            value:'стол',
+            direction:'vertical',
+            position:'1-5',
+            description:'Дел од мебелот, наменет за седење со или без потпирач'
+        },
+    ],
+    [
+        {
+            value:'балон',
+            direction:'horizontal',
+            position:'2-2',
+            description:'Еластично, елипсовидно тело, исполнето со воздух за да може да лета, може да се движи високо над земјата'
+        },
+        {
+            value:'азбука',
+            direction:'vertical',
+            position:'2-3',
+            description:'Утврден ред на буквите за гласовниот систем на еден јазик'
+        },
+        {
+            value:'нога',
+            direction:'vertical',
+            position:'2-6',
+            description:'Еден од деловите на телото на човекот и на животните со кој се оди.'
+        },
+    ],
+    [
+        {
+            value:'гавран',
+            direction:'horizontal',
+            position:'3-5',
+            description:'Вид птица со црни сјајни пердуви и силен клун'
+        },
+        {
+            value:'трон',
+            direction:'horizontal',
+            position:'7-3',
+            description:'Свечен стол на монарх или на црковен великодостојник, престол'
+        },
+        {
+            value:'прстен',
+            direction:'vertical',
+            position:'4-3',
+            description:'Обрач од метал, најчесто скапоцен, што се носи на прст како накит'
+        },
+        {
+            value:'глагол',
+            direction:'vertical',
+            position:'3-5',
+            description:'Вид менлив збор што означува дејство или состојба: оди; спие; пее; тагува и сл'
+        },
+    ],
+    [
+        {
+            value:'компјутер',
+            direction:'horizontal',
+            position:'2-1',
+            description:'Електронски уред способен да обработува податоци и да извршува различни задачи многу точно; електронски сметач'
+        },
+        {
+            value:'мачка',
+            direction:'horizontal',
+            position:'5-1',
+            description:'Домашно животно што лови глувци'
+        },
+        {
+            value:'очила',
+            direction:'horizontal',
+            position:'7-1',
+            description:'Оптичко помагало за подобрување на видот или за заштита на очите'
+        },
+        {
+            value:'пенкало',
+            direction:'vertical',
+            position:'2-4',
+            description:'Алатка со перо или со метален врв за пишување'
+        },
+        {
+            value:'шестар',
+            direction:'vertical',
+            position:'1-8',
+            description:'Направа за цртање кругови, кружни линии или за мерење мала должина и широчина'
+        }
+    ],
+    [
+        {
+            value:'семејство',
+            direction:'horizontal',
+            position:'2-1',
+            description:'Општествена единица што се состои од лица обединети со брак, крвно сродство или со усвојување и што претставува едно домаќинство; фамилија'
+        },
+        {
+            value:'еж',
+            direction:'horizontal',
+            position:'4-9',
+            description:'Мало животно од редот на цицачите, со боцки, игли по грбот'
+        },
+        {
+            value:'остров',
+            direction:'horizontal',
+            position:'8-2',
+            description:'Дел од копно од сите страни опкружено со вода'
+        },
+        {
+            value:'јупитер',
+            direction:'vertical',
+            position:'2-5',
+            description:'Најголема планета во Сончевиот Систем.Петта по ред'
+        },
+        {
+            value:'трева',
+            direction:'vertical',
+            position:'2-7',
+            description:'Општ назив за зелјеста едногодишна или повеќегодишна билка '
+        },
+        {
+            value:'океан',
+            direction:'vertical',
+            position:'2-9',
+            description:'Голема водена површина меѓу континентите'
+        }
+    ],
+    [
+        {
+            value:'орман',
+            direction:'horizontal',
+            position:'2-1',
+            description:'Дел од мебел, шкаф'
+        },
+        {
+            value:'авион',
+            direction:'horizontal',
+            position:'5-5',
+            description:'Метална направа за летање што ја движи мотор, а се одржува во воздух со помош на крила'
+        },
+        {
+            value:'ринг',
+            direction:'horizontal',
+            position:'6-1',
+            description:'Ограден простор за натпревари во бокс и во борење'
+        },
+        {
+            value:'бубамара',
+            direction:'horizontal',
+            position:'10-2',
+            description:'Инсект тврдокрилец со црвена или жолта боја и со црни дамки'
+        },
+        {
+            value:'боксер',
+            direction:'vertical',
+            position:'1-1',
+            description:'Спортист што се занимава со бокс'
+        },
+        {
+            value:'нива',
+            direction:'vertical',
+            position:'2-5',
+            description:'Определена земјишна површина што се обработува со орање, сеење, садење'
+        },
+        {
+            value:'виолина',
+            direction:'vertical',
+            position:'4-7',
+            description:'Гудачки инструмент со четири жици, највисок според тонскиот регистар од сите гудачки инструменти'
+        },
+        {
+            value:'оган',
+            direction:'vertical',
+            position:'2-9',
+            description:'Запалена материја (дрва, јаглен и др.) која горејќи развива топлина, светлина и чад'
+        },
+    ],
+    [
+        {
+            value:'емпатичен',
+            direction:'horizontal',
+            position:'1-1',
+            description:'Човек способен да се сочувствува со други луѓе.'
+        },
+        {
+            value:'воз',
+            direction:'horizontal',
+            position:'3-7',
+            description:'Низа од железнички коли, вагони што ги влече локомотива'
+        },
+        {
+            value:'елка',
+            direction:'vertical',
+            position:'1-1',
+            description:'Иглолисно зимзелено дрво со конусна круна'
+        },
+        {
+            value:'поговорка',
+            direction:'vertical',
+            position:'1-3',
+            description:'Општопозната куса, често синтаксички неоформена, изрека со преносно значење, која се искажува како израз на народната мудрост'
+        },
+        {
+            value:'товар',
+            direction:'vertical',
+            position:'1-5',
+            description:'Стока или тоа што е натоварено на товарно животно, превозно средство и сл'
+        },
+        {
+            value:'човек',
+            direction:'vertical',
+            position:'1-7',
+            description:'Суштество способно да мисли и да зборува, да создава и да користи орудија за работа и да води организиран општествен живот'
+        }
+    ],
+    [
+        {
+            value:'антагонист',
+            direction:'horizontal',
+            position:'2-1',
+            description:'Непомирлив противник, непријател, соперник'
+        },
+        {
+            value:'дедо',
+            direction:'horizontal',
+            position:'7-3',
+            description:'Татко на еден од родителите'
+        },
+        {
+            value:'согласка',
+            direction:'horizontal',
+            position:'9-3',
+            description:'Глас што се изговара со шум и обично не може сам да образува слог. Спротивно од самогласка'
+        },
+        {
+            value:'стјуардеса',
+            direction:'vertical',
+            position:'1-3',
+            description:'Келнер на патнички брод или авион. Женски род'
+        },
+        {
+            value:'игла',
+            direction:'vertical',
+            position:'2-8',
+            description:'Тенка метална прачка со остар врв на едниот крај и со мало дупче на другиот за вденување конец'
+        },
+        {
+            value:'точка',
+            direction:'vertical',
+            position:'2-10',
+            description:'Интерпункциски знак кој се става на крајот од расказна реченица'
+        }
+    ]
+];
+
+var focusWord
+        var username='';
+        var password='';
+    var currentLevel=7;
+        function show(element){
+            var characters=element.value.split('');
+
+            temp=element.position.split('-');
+            if(element.direction=='vertical'){
+                    temp[0]=parseInt(temp[0]);
+                    for(j=0;j<element.value.length;j++){
+                        newId=temp[0]+"-"+temp[1];
+                        temp[0]++;
+                        // console.log(newId)
+                        $('#'+newId).text(characters[j])
+                    }
+                }else{
+                    temp[1]=parseInt(temp[1]);
+                    for(j=0;j<element.value.length;j++){
+                        newId=temp[0]+"-"+temp[1];
+                        temp[1]++;
+                        // console.log(newId)не
+                        $('#'+newId).text(characters[j])
+                    }
+                }
+                $('#test').val('')
+        }
+
+        function update(focusWord){
+            $('td').removeClass('first').removeClass('rest');
+            $('#'+focusWord.position).addClass('first')
+            temp=focusWord.position.split('-');
+                if(focusWord.direction=='vertical'){
+                    temp[0]=parseInt(temp[0])+1;
+                    for(j=0;j<focusWord.value.length-1;j++){
+                        newId=temp[0]+"-"+temp[1];
+                        temp[0]++;
+                        // console.log(newId)
+                        $('#'+newId).addClass('rest')
+                    }
+                }else{
+                    temp[1]=parseInt(temp[1])+1;
+                    for(j=0;j<focusWord.value.length-1;j++){
+                        newId=temp[0]+"-"+temp[1];
+                        temp[1]++;
+                        // console.log(newId)
+                        $('#'+newId).addClass('rest')
+                    }
+                }
+                $('#desc').text(focusWord.description)
+        }
+        function makeCurrentWords(){
+            currentLevel+=1;
+            $('#nivo').text(currentLevel);
+            currentWords=[];
+            dictionary[currentLevel-1].forEach((element,index)=>{
+                currentWords.push(element);
+            })
+            for(i=0;i<currentWords.length;i++){
+                temp=currentWords[i].position.split('-');
+                if(currentWords[i].direction=='vertical'){
+                    temp[0]=parseInt(temp[0]);
+                    for(j=0;j<currentWords[i].value.length;j++){
+                        newId=temp[0]+"-"+temp[1];
+                        temp[0]++;
+                        // console.log(newId)
+                        $('#'+newId).addClass('word')
+                    }
+                }else{
+                    temp[1]=parseInt(temp[1]);
+                    for(j=0;j<currentWords[i].value.length;j++){
+                        newId=temp[0]+"-"+temp[1];
+                        temp[1]++;
+                        // console.log(newId)
+                        $('#'+newId).addClass('word')
+                    }
+                }
+            }
+            
+            focusWord=currentWords[0];
+            update(focusWord);
+            return currentWords;
+        }
+        function victory(){
+            $('#nivo-parent').hide();
+            $('#left').attr('disabled',true)
+            $('#right').attr('disabled',true)
+            $('#container,#controls').hide()
+            $('#finish').show()
+        }
+
+        function nextLvl(){
+            $('td').removeClass('word').text('')
+            
+            makeCurrentWords();
+            // $('#right').trigger('click');
+            setTimeout(()=>{
+                $('body').css('animation','none')
+            },2000)
+            // $('body').css('animation','none')
+            
+        }
+        $(document).ready(function () {
+
+            // $('#extra,#container,#controls').removeClass('blur')//da se trgne
+            $('#finish').hide()
+            $('#login').dialog({
+                draggable:false,
+                autoOpen:true,//ttttttrrrrrrrrrrrruuuuuuueeeeeeee
+                modal:true,
+                resizable:false,
+                title:'Најави се',
+                buttons :  { 
+                    "MyButton" : {
+                        text: "Најава",
+                        id: "najava",
+                        click: function(){
+                            if($('#pass').val().length>=5){
+                                username=$('#name').val();
+                                password=$('#pass').val();
+                                $('#errormsg').addClass('errormsg');
+                                $('#name').val('')
+                                $('#pass').val('');
+                                $('#ime').text(username);
+                                $('#extra,#container,#controls').removeClass('blur')
+                                document.getElementById('zvuk').play()
+                                $('#login').dialog('close') 
+                                $('#test').focus();
+                                
+                            }else{
+                                $('#errormsg').removeClass('errormsg');
+                                $('#pass').val('')
+                            }
+                        }   
+                     } 
+                  },
+                
+            })
+            
+            $('#pass').on('keyup',function(event){
+                if(event.which==13){
+                    $('#najava').trigger('click');
+                }
+            })
+            $.each($('td'),function(index,element){
+                var id=($(element).parent().index()+1)+"-"+($(element).index()+1);
+                $(element).attr('id',id)
+            })
+            
+            currentWords=makeCurrentWords();
+            focusWord=currentWords[0];
+            update(focusWord);
+            $('#test').on('keyup',function(event){
+                if(event.which==13){
+                    $('#clicker').trigger('click');
+                }
+            })
+            $('#clicker').on('click',function(){
+                $attempt=$('#test');
+                if($attempt.val().toLowerCase()==focusWord.value){
+                    new Audio('correct.mp3').play()
+                    show(focusWord);
+                    currentWords.splice(currentWords['indexOf'](focusWord),1);
+                    if(currentWords.length==0){
+                        
+                        $('td').removeClass('first rest');
+                        
+                        
+                        $('body').css('animation','animate 2s normal')
+                        $('body').css('animation','animate 2s reverse')
+                        if(currentLevel==dictionary.length){
+                            victory();
+
+                            return;
+                        }
+                            nextLvl()
+                            // $('body').css('animation','none')
+                    }
+                    if(currentWords.length!=0 && currentWords.length!=dictionary[currentLevel-1].length){
+                        $('#right').trigger('click');
+                    }
+                }else{
+                    new Audio('wrong.mp3').play()
+                }
+                $attempt.val('')
+            })
+            $('#left').on('click',function(){
+                inx=currentWords['indexOf'](focusWord)-1;
+                if(inx<0){
+                    inx=currentWords.length-1;
+                }
+                focusWord=currentWords[inx];
+    
+                update(focusWord);
+                $('#test').focus();
+            })
+            $('#right').on('click',function(){
+                inx=(currentWords['indexOf'](focusWord)+1)%currentWords.length;
+                focusWord=currentWords[inx];
+    
+
+                update(focusWord);
+                $('#test').focus();
+            })
+            $('#sound').on('click',function(){
+                if(this.src.match('mute-dark.png')){
+                    this.src='speaker-dark.png';
+                
+                    document.getElementById('zvuk').play()                    
+                } else if(this.src.match('speaker-dark.png')){
+            
+                    this.src='mute-dark.png'
+                
+                    document.getElementById('zvuk').pause() 
+                }else if(this.src.match('speaker-light.png')){
+                    
+                    this.src='mute-light.png'
+                
+                    document.getElementById('zvuk').pause() 
+                }else if(this.src.match('mute-light.png')){
+                    
+                    this.src='speaker-light.png'
+                    
+                    document.getElementById('zvuk').play() 
+                }
+            })
+            $('#light').on('click',function(){
+
+                if($('body').css('backgroundImage').match('dark')){
+                    $('body').css('backgroundImage',"url('background-light.jpeg')")
+                }else{
+                    $('body').css('backgroundImage',"url('background-dark.jpeg')")
+
+                }
+                $('#extra p').toggleClass('dark')
+
+                y=document.getElementById('congrat');
+                if(y.src.match('congratulations-dark.png')){
+                    y.src='congratulations-light.png';
+                }else{
+                    y.src='congratulations-dark.png';
+                }
+                
+                $('#desc').toggleClass('dark')
+
+                $('td').toggleClass('dark')
+                
+                x=document.getElementById('sound');
+                if(x.src.match('mute-dark.png')){
+                    x.src='mute-light.png';
+                
+                    // document.getElementById('zvuk').play()                    
+                } else if(x.src.match('speaker-dark.png')){
+            
+                    x.src='speaker-light.png'
+                
+                    // document.getElementById('zvuk').pause() 
+                }else if(x.src.match('speaker-light.png')){
+                    
+                    x.src='speaker-dark.png'
+                
+                    // document.getElementById('zvuk').pause() 
+                }else if(x.src.match('mute-light.png')){
+                    
+                    x.src='mute-dark.png'
+                    
+                    // document.getElementById('zvuk').play() 
+                }
+            })
+            $('#playagain').on('click',function(){
+                $('body').css('animation','none')
+                // setTimeout(()=>{
+                // },2000)
+                setTimeout(()=>{
+                    $('body').css('animation','animate 2s normal')
+                    $('body').css('animation','animate 2s reverse')
+                    currentLevel=0;
+                    $('#finish').hide()
+                    $('#nivo-parent').show();
+                    $('#container,#controls').show()
+                    nextLvl();
+                    $('#test').focus()
+                },100)
+                
+            })
+            
+        })
